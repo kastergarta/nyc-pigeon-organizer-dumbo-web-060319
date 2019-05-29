c2b18783effc9e5a63962401af31bf65b5e95772
Dennis <dennis.basyrov@gmail.com>
2019-05-29 03:17:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, h), result|
     h.each do |value, names|
       names.each do |name|
-        result[name] ||= {}
-        result[name][key] ||= []
-        result[name][key] << value.to_s
+      result[name] ||= {}
+      result[name][key] ||= []
+      result[name][key] << value.to_s
       end
     end
   end
